93f5290c8c3945960dd76059d1698fa296276261
Javierflipswitch <Javier.Valles@flipswitch.com>
2016-04-20 11:18:21 -0700

    Added files via upload

--- view1/view1Controller.js
+++ view1/view1Controller.js
@@ -3,9 +3,9 @@ var app = angular.module('myApp.view1', []);
 app.controller('view1Controller', function($scope) {
 
   $scope.character = {
-    type: '', 
-    personality: '',
-    job: ''      
+    characterTypes: '', 
+    personalityTypes: '',
+    jobTypes: ''      
   };
   
   $scope.characterTypes = [
@@ -15,5 +15,11 @@ app.controller('view1Controller', function($scope) {
   $scope.personalityTypes = [ 
       {id: 1, value: 'Good'}, 
       {id:2, value: 'Evil'}];
+      
+  $scope.jobTypes = [
+    {id: 1, value: 'Paladin' },
+    {id: 2, value: 'Brute'},
+    {id: 3, value: 'Sorcerer'},
+    {id: 4, value: 'Witch'}];
   
-});
+}); 
