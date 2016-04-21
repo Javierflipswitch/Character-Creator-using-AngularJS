acce3de614d9f61ecb28891a07b91da2cd839f47
Javierflipswitch <Javier.Valles@flipswitch.com>
2016-04-21 11:52:38 -0700

    Added files via upload

--- view1/view1Controller.js
+++ view1/view1Controller.js
@@ -1,25 +1,59 @@
 var app = angular.module('myApp.view1', []);
 
-app.controller('view1Controller', function($scope) {
+app.controller('view1Controller', function() {
 
-  $scope.character = {
-    characterTypes: '', 
-    personalityTypes: '',
-    jobTypes: ''      
+  var vm = this;
+
+  vm.character = {};
+  vm.master = {};
+ 
+  vm.update = function(character) {
+      vm.master = angular.copy(vm.character);
+      vm.character = {};
   };
   
-  $scope.characterTypes = [
-      {id:1, value: 'Knight'},
-      {id:2, value: 'Wizard'}];
+  vm.reset = function(myForm) {
+      vm.character = angular.copy(vm.master);
+  };
+  
+  vm.characterTypes = [
+      {id: 1, value: 'Knight'},
+      {id: 2, value: 'Wizard'}];
 
-  $scope.personalityTypes = [ 
+  vm.personalityTypes = [ 
       {id: 1, value: 'Good'}, 
-      {id:2, value: 'Evil'}];
+      {id: 2, value: 'Evil'}];
       
-  $scope.jobTypes = [
+  vm.jobTypes = [
     {id: 1, value: 'Paladin' },
     {id: 2, value: 'Brute'},
     {id: 3, value: 'Sorcerer'},
     {id: 4, value: 'Witch'}];
   
+ function getJobTypeById(id){
+    return vm.jobTypes.filter(function(e) { return e.id == id})[0].value;
+ }
+
+  vm.assignJob = function() {
+      var ctype = vm.character.characterType;
+      var ptype = vm.character.personalityType;
+        if(ctype == 1 && ptype == 1)
+        {
+           return getJobTypeById(1);
+        }
+        else if (ctype == 1 && ptype == 2)
+        {
+            return getJobTypeById(2);
+        }
+           else if (ctype == 2 && ptype == 1)
+        {
+            return getJobTypeById(3);
+        }
+           else if (ctype == 2 && ptype == 2)
+        {
+            return getJobTypeById(4);
+        }
+  }  
+  
+  vm.character.jobTypes = vm.assignJob();
 }); 
